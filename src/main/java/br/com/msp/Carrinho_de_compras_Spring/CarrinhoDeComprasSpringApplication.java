664451d5940af35d5ed9eb71486afa114cc62511
Minessp <minestadionchannel@gmail.com>
2025-03-01 13:50:57 -0300

    First commit

---
+++ src/main/java/br/com/msp/Carrinho_de_compras_Spring/CarrinhoDeComprasSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.msp.Carrinho_de_compras_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoDeComprasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoDeComprasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/msp/Carrinho_de_compras_Spring/CarrinhoDeComprasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msp.Carrinho_de_compras_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoDeComprasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
