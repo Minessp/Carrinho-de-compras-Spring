59b7b91541fed7b65fb80916b5e2c166cff954f4
Minessp <minestadionchannel@gmail.com>
2025-03-01 15:05:48 -0300

    Adicionado conexão com DB PostgreSQL para requisições via Postman

--- src/main/java/br/com/msp/carrinho_de_compras_spring/entity/Product.java
+++ src/main/java/br/com/msp/carrinho_de_compras_spring/entity/Product.java
@@ -1,17 +1,23 @@
 package br.com.msp.carrinho_de_compras_spring.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "produto")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Integer id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private Double preco;
+
+    @Column
     private Integer quantidade;
 
     public Integer getId() {
